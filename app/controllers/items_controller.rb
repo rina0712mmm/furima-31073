0bdcf25966e2c275a7c2565f559ee3c521248926
rina0712mmm <rina0712.mmm@gmail.com>
2020-11-02 19:12:28 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
   end
 
   def show
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 end
