0104baf7f410fe0e3ed1c60ba4c0dae5ff4400f8
rina0712mmm <rina0712.mmm@gmail.com>
2020-11-06 15:57:23 +0900

    robocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,24 +28,22 @@ class ItemsController < ApplicationController
   end
 
   def update
-      @item= Item.find(params[:id])
+    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
-   end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_detail, :category_id, :condition_id, :postage_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
-    end
+  end
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
